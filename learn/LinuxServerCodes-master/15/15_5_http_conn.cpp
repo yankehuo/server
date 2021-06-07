c8e656ec028121642cda3440ea4970a7f87d1ceb
yankehuo <qingyankehuo@gmail.com>
2021-06-07 23:26:22 +0800

    20210607232623

---
+++ learn/LinuxServerCodes-master/15/15_5_http_conn.cpp
@@ -0,0 +1,44 @@
+#include "15_4_http_conn.h"
+const char *ok_200_title = "OK";
+const char *error_400_title = "Bad Request";
+const char *error_400_form = "Your request has bad syntax or is inherently impossible to satisfy.\n";
+const char *error_403_title = "Forbidden";
+const char *error_403_form = "You do not have permission to get file from this server.\n";
+const char *error_404_title = "Not Found";
+const char *error_404_form = "The requested file was not found on this server.\n";
+const char *error_500_title = "Internal Error";
+const char *error_500_form = "There was an unusual problem serving the requested file.\n";
+const char *doc_root = "/var/www/html";
+
+int setnonblocking(int fd) {
+	int old_option = fcntl(fd, F_GETFL);
+	int new_option = old_option | O_NONBLOCK;
+	fcntl(fd, F_SETFL, new_option);
+	return old_option;
+}
+
+void addfd(int epollfd, int fd, bool one_shot) {
+	epoll_event event;
+	event.data.fd = fd;
+	// why?
+	event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+	if (one_shot) {
+		event.events |= EPOLLONESHOT;
+	}
+	epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+	setnonblocking(fd);
+}
+
+void remove(int epollfd, int fd) {
+	epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+	close(fd);
+}
+
+void modfd(int epollfd, int fd, int ev) {
+	epoll_event event;
+	event.data.fd = fd;
+	event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+	epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
+}
+
+ 
