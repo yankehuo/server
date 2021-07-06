51e12535ab153ee57332edb21df3a7a4e9ee36ef
yankehuo <qingyankehuo@gmail.com>
2021-07-06 23:56:19 +0800

    evening

---
+++ learn/thread_reactor/server/epoller.h
@@ -0,0 +1,32 @@
+#ifndef EPOLLER_H
+#define EPOLLER_H
+
+// epoll_ctl
+#include <sys/epoll.h>
+// fcntl
+#include <fcntl.h>
+// close
+#include <unistd.h>
+#include <cassert>
+#include <vector>
+#include <errno.h>
+
+class Epoller {
+private:
+	int epollFd_;
+	std::vector<struct epoll_event> events_;
+public:
+	explicit Epoller(int maxEvent = 1024);
+	~Epoller();
+	bool AddFd(int fd, uint32_t events);
+	bool ModFd(int fd, uint32_t events);
+	bool DelFd(int fd);
+
+	int Wait(int timeoutMs = -1);
+	int GetEventFd(size_t i) const;
+	uint32_t GetEvents(size_t i) const;
+};
+
+
+
+#endif
