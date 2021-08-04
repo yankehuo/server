278f4e552f9147ca149858037584273b428c9e78
yankehuo <qingyankehuo@gmail.com>
2021-08-04 11:51:14 +0800

    proacotr and reactor

---
+++ logbq/log.h
@@ -0,0 +1,81 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <mutex>
+#include <string>
+#include <thread>
+#include <sys/time.h>
+#include <cassert>
+#include <cstring>
+// va_start va_end
+#include <cstdarg>
+// mkdir
+#include <sys/stat.h>
+#include "blockqueue.h"
+#include "../buffer/buffer.h"
+
+class Log {
+private:
+	static const int LOG_PATH_LEN = 256;
+	static const int LOG_NAME_LEN = 256;
+	static const int MAX_LINES = 50000;
+
+	const char *path_;
+	const char *suffix_;
+
+	int lineCount_;
+	int toDay_;
+
+	bool isOpen_;
+
+	Buffer buff_;
+	int level_;
+	bool isAsync_;
+
+	FILE *fp_;
+	std::unique_ptr<BlockDeque<std::string>> deque_;
+	std::unique_ptr<std::thread> writeThread_;
+	std::mutex mtx_;
+public:
+	void init(int level, const char *path = "./log", const char *suffix = ".log", int maxQueueCapacity = 1024);
+
+	// singleton lazy mode
+	static Log *Instance() {
+		static Log instance;
+		return &instance;
+	};
+
+	static void FlushLogThread();
+
+	void write(int level, const char *format, ...);
+	void flush();
+
+	int GetLevel();
+	void SetLevel(int level);
+	bool IsOpen() {
+		return isOpen_;
+	}
+private:
+	// singleton
+	Log();
+	void AppendLogLevelTitle_(int level);
+	// ~Log();
+	~Log();
+	void AsynWrite_();
+};
+
+
+ #define LOG_BASE(level, format, ...) \
+	do {\
+		Log *log = Log::Instance();\
+		if (log->IsOpen() && log->GetLevel() <= level) {\
+			log->write(level, format, ##__VA_ARGS__);\
+			log->flush();\
+		}\
+	} while (0);
+#define LOG_DEBUG(format, ...) do {LOG_BASE(0, format, ##__VA_ARGS__)} while (0);
+#define LOG_INFO(format, ...) do {LOG_BASE(1, format, ##__VA_ARGS__)} while (0);
+#define LOG_WARN(format, ...) do {LOG_BASE(2, format, ##__VA_ARGS__)} while (0);
+#define LOG_ERROR(format, ...) do {LOG_BASE(3, format, ##__VA_ARGS__)} while (0);
+
+#endif
