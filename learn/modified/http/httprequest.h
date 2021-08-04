d93d9bc6fe7080d1f82e05e9d2e424b37e1dca65
yankehuo <qingyankehuo@gmail.com>
2021-08-04 11:42:44 +0800

    proactor

---
+++ learn/modified/http/httprequest.h
@@ -0,0 +1,61 @@
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+// regular expressions
+#include <regex>
+#include <cerrno>
+
+#include "../buffer/buffer.h"
+#include "../logbq/log.h"
+
+class HttpRequest {
+public:
+	enum PARSE_STATE {
+		REQUEST_LINE = 0,
+		HEADERS,
+		BODY,
+		FINISH,
+	};
+
+	enum HTTP_CODE {
+		NO_REQUEST = 0,
+		GET_REQUEST,
+		BAD_REQUEST,
+		NO_RESOURCE,
+		FORBIDDENT_REQUEST,
+		FILE_REQUEST,
+		INTERNAL_ERROR,
+		CLOSED_CONNECTION,
+	};
+
+	HttpRequest() {
+		Init();
+	}
+	~HttpRequest() = default;
+
+	void Init();
+	bool parse(Buffer &buff);
+
+	std::string path() const;
+	std::string &path();
+	std::string method() const;
+	std::string version() const;
+
+	bool IsKeepAlive() const;
+private:
+	bool ParseRequestLine_(const std::string &line);
+	void ParseHeader_(const std::string &line);
+	void ParseBody_(const std::string &line);
+	void ParsePath_();
+private:
+	PARSE_STATE state_;
+	std::string method_, path_, version_, body_;
+	std::unordered_map<std::string, std::string> header_;
+
+	static const std::unordered_set<std::string> DEFAULT_HTML;
+};
+
+#endif
